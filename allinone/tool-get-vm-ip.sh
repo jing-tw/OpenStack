401ea9f0c273d959ec0192b5cee4a4c44283f761
jing-tw <mqjing@gmail.com>
2016-04-16 02:39:19 +0800

    add get running vm ip information

---
+++ allinone/tool-get-vm-ip.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+subnet=`ip r | grep default | cut -d ' ' -f 3|awk -F '[.]' '{printf "%s.%s.%s.\n",$1,$2,$3}'`
+
+HostNameArray=($(vboxmanage list runningvms |cut -d" " -f 1))
+IPArray=($(vboxmanage list runningvms | sed -r 's/.*\{(.*)\}/\1/' | xargs -L1 -I {} vboxmanage guestproperty enumerate {} | grep "Net.*V4.*IP" | grep $subnet | awk -F '[, \" \" ]' '{print $5}'))
+
+for (( i=0; i<${#HostNameArray[@]}; i++)); do
+   echo "${HostNameArray[i]} => ${IPArray[i]} "
+done
